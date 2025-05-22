41675d1f349619fbded1f0b89bfa3b1640c07b0a
Matheus Souza <matheusouzafloriani@gmail.com>
2025-05-22 13:32:53 -0300

    refatora o App.tsx e Home para gerenciar o estado das tarefas, adicionando props de estado e setState

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
+import { useState } from 'react';
+import type { TaskStateModel } from './models/TaskStateeModel';
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 export function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  const [state, setState] = useState(initialState);
+
+  return <Home state={state} setState={setState} />;
 }
